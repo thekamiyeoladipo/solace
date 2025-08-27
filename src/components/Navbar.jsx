ca3bd55d6b0250689d90f31c164fa01bb4df6b91
kamiye <kamiye31@gmail.com>
2025-08-27 13:40:31 +0100

    Add Footer component and enhance Navbar with logo and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/solace logo.svg' // Adjust the path as necessary
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className="bg-[#E0EFFF] h-[100px] flex items-center sticky top-0 z-50 border-b border-dark">
+      <div className="container mx-auto flex justify-between items-center w-full px-8">
         
-    </div>
+        {/* Logo */}
+        <div className="flex-shrink-0">
+          <img src={logo} alt="Solace Logo" className="h-auto w-auto" />
+        </div>
+
+        {/* Links */}
+        <ul className="flex space-x-12 text-lg font-normal">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/career">Career</Link></li>
+          <li><Link to="/testimonials">Testimonials</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+        </ul>
+
+        {/* Button */}
+        <div>
+          <button className="bg-faintgray text-dark px-6 py-3 rounded-lg">
+            Get a Quote
+          </button>
+        </div>
+      </div>
+    </nav>
   )
 }
 
