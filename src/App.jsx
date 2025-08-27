832430fc74bc098a27bf95aa7df685a4f487e868
kamiye <kamiye31@gmail.com>
2025-08-27 12:28:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  
+  return (
+    <>
+      <div className='flex justify-center items-center flex-col'>
+        <h1 className='underline'>KAMIYE IS HERE</h1>
+        <p className='font-bold text-red-800'>Tailwindcss works just fine</p>
+        <h1 className='font-bold text-5xl underline tracking-wide'>
+          This is the solace app!
+        </h1>
+      </div>
+     
+    </>
+  )
+  
+}
+
+export default App
