f28ed4e94c8636f09c523405e20af65fc066e279
kamiye <kamiye31@gmail.com>
2025-09-01 09:36:02 +0100

    Add: dynamic interactivity to the breadcrumb component

---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,11 @@
+
+
+const BreadCrumb = ({ text }) => {
+  return (
+   <>
+    <p className="inline-block rounded-full px-8 py-1 text-white bg-green mb-4">{text}</p>
+   </>
+  )
+}
+
+export default BreadCrumb
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import BreadCrumb from "../components/BreadCrumb"
 
 const About = () => {
   return (
-    <div>
-      About Page
+    <section className="bg-white py-20">
+      <div className="container mx-auto px-8 text-center">
+        {/* Page Breadcrumb */}
+       <BreadCrumb text="Home / About Us" />
+        {/* Heading */}
+        <h1 className="text-8xl md:text-5xl font-black text-green mb-6">
+          About Our Company
+        </h1>
+
+        {/* Paragraph */}
+        <p className="text-sm text-dark/80 font-extrabold max-w-2xl mx-auto">
+          We value your feedback, questions, and inquiries. Whether you have a
+          project in mind, need assistance with our services, or simply want to
+          say hello, we’re here to assist you.
+        </p>
       </div>
-  )
+    </section>
+  );
 }
 
 export default About
