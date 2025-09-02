541c85963271527b68b219f0e86a51f9084b141a
kamiye <kamiye31@gmail.com>
2025-09-02 12:54:31 +0100

    Add: Carrer page dev started

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -2,10 +2,12 @@
 
 const BreadCrumb = ({ text }) => {
   return (
-   <>
-    <p className="inline-block rounded-full px-8 py-1 text-white bg-green mb-4">{text}</p>
-   </>
-  )
+    <>
+      <p className="inline-block rounded-full px-8 py-1 text-white bg-green mb-4">
+        {text}
+      </p>
+    </>
+  );
 }
 
 export default BreadCrumb
--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import BreadCrumb from "../components/BreadCrumb";
 
 const Career = () => {
   return (
-    <div>Career</div>
-  )
+    <>
+      <section className="py-20">
+        <div className="container mx-auto px-8 text-center">
+          <div>
+            {/* Page Breadcrumb */}
+            <BreadCrumb text="Home / Career" />
+            {/* Heading */}
+            <h1 className="text-8xl md:text-5xl font-black text-green mb-6">
+              Career
+            </h1>
+
+            {/* Paragraph */}
+            <p className="text-sm text-dark/80 font-extrabold max-w-2xl mx-auto">
+              We’re looking for talented individuals who share our dedication to
+              excellence and creativity. Explore exciting career opportunities
+              and be part of our team
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Career
